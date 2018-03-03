a86a5cb33343e7174566881fd0e138f2c2c29cf0
Tom August <tomaug@ceh.ac.uk>
2018-03-03 22:03:04 +0000

    save latest test script

--- misc/test_trees.R
+++ misc/test_trees.R
@@ -7,10 +7,10 @@ library(CEHcraft)
 # 23 seconds 
 
 system.time({postcode_map(postcode = 'LA229LB',
-                          outputDir = 'C:/Users/tomaug/AppData/Roaming/.minecraft/saves',
+                          outputDir = 'C:/Users/Tom August/AppData/Roaming/.minecraft/saves',
                           radius = 2500,
                           verbose = TRUE)})
 
-overview(savePath = 'C:/Users/tomaug/AppData/Roaming/.minecraft/saves/LA229LB', 
+overview(savePath = 'C:/Users/Tom August/AppData/Roaming/.minecraft/saves/LA229LB', 
          outPath = tempdir(),
          launch = TRUE)
